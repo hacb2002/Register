cb1af12695cc8c75c1c93cf7f92211c0e13fcea1
Hector Alexis Crisostomo Bonilla <al2213031453@azc.uam.mx>
2024-08-08 12:33:23 -0600

    Se considera subir el registro con int y con registros, mas una captura

---
+++ registerInt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char* argv[]){
+    if(argc!=2){
+    	printf("Forma de uso: %s ValorLong \n", argv[0]);
+	return -1;
+    }
+    long max = atol(argv[1]);
+
+    //Calcula el tiempo de ejecución
+    clock_t t = clock();
+    printf("Inicio\n");
+
+    int n = 0;
+    for(n = 0; n<(int)max; n++);
+    printf("Fin\n");
+    t = clock() - t;
+    double tiempo = ((double)t)/CLOCKS_PER_SEC; //Calcula el tiempo transcurrido
+    printf("tiempo transcurrido %f segundos\n", tiempo);
+    return 0;
+}
